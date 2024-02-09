58fdbc685304648abc290a60b0de8578335686da
jump-kick <27829245+jump-kick@users.noreply.github.com>
2024-02-08 20:31:43 -0500

    Changed to a read only function to make it easier to load and reload, avoiding a nasty require caching bug

--- data/products.js
+++ data/products.js
@@ -1,19 +1,21 @@
 //Simple array to provide seed product data for our application
-productsArray = [{
-    price: 6.00,
-    name: "Spider-Man comic book",
-    description: "Spidey vs. Venom: who will win??  Also: Aunt May's 100th birthday!",
-    id: 1
-}, {
-    price: 600.00,
-    name: "iPhone",
-    description: "Expensive smart phone",
-    id: 2
-}, {
-    price: 20.00,
-    name: "Bluetooth ear buds",
-    description: "Cordless rechargeable earbuds; includes charging cord",
-    id: 3
-}];
+function load(){
+    return [{
+        price: 6.00,
+        name: "Spider-Man comic book",
+        description: "Spidey vs. Venom: who will win??  Also: Aunt May's 100th birthday!",
+        id: 1
+    }, {
+        price: 600.00,
+        name: "iPhone",
+        description: "Expensive smart phone",
+        id: 2
+    }, {
+        price: 20.00,
+        name: "Bluetooth ear buds",
+        description: "Cordless rechargeable earbuds; includes charging cord",
+        id: 3
+    }];
+}
 
-module.exports = {productsArray}
+module.exports = {load}
